e8ccaa1f23ae829e7489391fdaae4637b21898bb
nurayy12 <nuryy1203@gmail.com>
2025-05-24 13:49:28 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['user_id'])) {
 </head>
 <body>
     <h1>Welcome, <?= $_SESSION['name'] ?>!</h1>
-    <p><a href="rentals.php">Rent a Vehicle</a> | <a href="logout.php">Logout</a></p>
+    <p><a href="rentals.php" class="btn-primary" >Rent a Vehicle</a> | <a href="logout.php">Logout</a></p>
     <p><a href="rented_vehicles.php" class="btn-primary">My Rental History</a></p>
    
 </body>
